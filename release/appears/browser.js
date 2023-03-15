02220b0ca008845eded23decad48c52bb44bfb94
ARam <dongjianye@nears.cn>
2023-03-16 02:47:09 +0800

    release(0.0.4): release 0.0.4

--- release/appears/browser.js
+++ release/appears/browser.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.appear = void 0;
 const appear = (log, server) => {
     fetch(server, {
+        method: 'POST',
         body: JSON.stringify({
             console: log,
         }),
